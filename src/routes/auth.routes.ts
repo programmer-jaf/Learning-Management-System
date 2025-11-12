7c1787d87bd2be7392f8003f99852e07cb4b77fc
Programmer-Jaf <programmerjaf@gmail.com>
2025-11-12 12:50:08 +0600

    feat(refreshToken): implement refreshToken Feature

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -16,6 +16,7 @@ import { signinController } from '@modules/auth/v1/controller/signin.controller'
 import { signoutController } from '@modules/auth/v1/controller/signout.controller';
 import { isAuthenticated } from '@middlewares/auth.middlewares';
 import { authMeController } from '@modules/auth/v1/controller/auth.me.controller';
+import { refreshAccessTokenController } from '@modules/auth/v1/controller/refreshAccessToken.controller';
 
 // --------------------------------------------------
 // Initialize Router
@@ -31,7 +32,7 @@ router.post('/signout', isAuthenticated, signoutController);
 router.post('/me', isAuthenticated, authMeController);
 // router.post('/forgot-password');
 // router.post('/reset-password');
-// router.post('/refresh-token');
+router.post('/refresh-token', isAuthenticated, refreshAccessTokenController);
 
 // --------------------------------------------------
 // Export Router
